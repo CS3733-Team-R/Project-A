7608780ac4282989d037d27244caf68a580f961e
Leonid Belyi <leonid@ac93.org>
2018-03-15 15:47:20 -0400

    Fixed indentation

--- src/TeamList.java
+++ src/TeamList.java
@@ -1,11 +1,11 @@
 public class TeamList {
-  public static void main(String [] args)
-  {
-    System.out.println("Tim Winters");
-    System.out.println("Leo Belyi");
-    System.out.println("Rory Sullivan");
-    System.out.println("Joe Bartone");
-    System.out.println("Tabitha Gibbs");
-  }
+    public static void main(String [] args)
+    {
+        System.out.println("Tim Winters");
+        System.out.println("Leo Belyi");
+        System.out.println("Rory Sullivan");
+        System.out.println("Joe Bartone");
+        System.out.println("Tabitha Gibbs");
+    }
 }
 
