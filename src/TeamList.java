c5de91054fee65186f22c66db6011ed40239b886
jlfong <jlfong@wpi.edu>
2018-03-15 15:43:13 -0400

    test

--- src/TeamList.java
+++ src/TeamList.java
@@ -2,5 +2,6 @@ public class TeamList {
     public static void main(String[] args) {
         System.out.println("Tim Winters");
         System.out.println("Rory Sullivan");
+        System.out.println("Janette Fong");
     }
 }
