3c03ab6944d38f6f0c75787763800551c522a62e
Tim Winters <twinters@wpi.edu>
2018-03-15 19:48:16 -0400

    Add year

--- src/TeamList.java
+++ src/TeamList.java
@@ -1,7 +1,7 @@
 public class TeamList {
   public static void main(String [] args)
   {
-    System.out.println("Tim Winters");
+    System.out.println("Tim Winters. Sophomore");
     System.out.println("Leo Belyi");
     System.out.println("Rory Sullivan");
     System.out.println("Meghan Flynn");
