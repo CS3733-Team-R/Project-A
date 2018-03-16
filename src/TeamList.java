f29766f020964dd6cd445ae1beeecce96362e37d
Leonid Belyi <leonid@ac93.org>
2018-03-15 21:04:27 -0400

    Update Leo name

--- src/TeamList.java
+++ src/TeamList.java
@@ -2,7 +2,7 @@ public class TeamList {
   public static void main(String [] args)
   {
     System.out.println("Tim Winters");
-    System.out.println("Leo Belyi");
+    System.out.println("Leonid Belyi");
     System.out.println("Rory Sullivan");
     System.out.println("Meghan Flynn");
     System.out.println("Joe Bartone");
