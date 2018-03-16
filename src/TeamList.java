22312c958647ffcba62be464c61dd1a34b2e9b43
alppiskin <apiskin@wpi.edu>
2018-03-16 01:16:13 -0400

    Alp Piskin added name

--- src/TeamList.java
+++ src/TeamList.java
@@ -7,8 +7,9 @@ public class TeamList {
     System.out.println("Meghan Flynn");
     System.out.println("Joe Bartone");
     System.out.println("Tabitha Gibbs");
-    System.out.println("Janette Fong");
+    System.out.println("Janette Fong. Sophomore");
     System.out.println("Sylvia van der Weide");
+    System.out.println("Mitchell Burns. Sophomore");
     System.out.println("Alp Piskin. Sophomore");
   }
 }
